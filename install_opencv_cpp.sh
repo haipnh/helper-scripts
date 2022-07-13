ed222eb13893a3a81744b49f68b4db615b85381c
Hoang Hai <haiphamnguyenhoang@gmail.com>
2022-07-13 14:47:14 +0800

    Update install_opencv_cpp.sh

--- install_opencv_cpp.sh
+++ install_opencv_cpp.sh
@@ -1,6 +1,5 @@
 sudo add-apt-repository "deb http://security.ubuntu.com/ubuntu xenial-security main"
 
-
 sudo apt install -y build-essential cmake wget unzip checkinstall pkg-config yasm gfortran software-properties-common \
 jasper ffmpeg python3-minimal python3 python3-pip doxygen libeigen3-dev libgtk2.0-dev libgtk-3-dev libjpeg8-dev libpng-dev \
 libjasper1 libjasper-dev libtiff-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine2-dev libv4l-dev \
